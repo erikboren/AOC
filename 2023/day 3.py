01b677fa554c587143ed4de54a882278b68ffe8a
erikboren <erik.boren@gmail.com>
2023-12-14 19:03:31 +0100

    Day 3 + part 1 day 4 completed :D

--- 2023/day 3.py
+++ 2023/day 3.py
@@ -2,6 +2,7 @@ import re
 
 with open("day 3 input.txt") as my_file:
     data = my_file.read().splitlines()
+# Part 1
 
 def find_neighbours(y, x1, x2):
     
@@ -24,5 +25,38 @@ for y, line in enumerate(data):
             parts.append(int(hit.group()))
 print(sum(parts))
 
+def find_parts(y,x):
+    def check(y,x):
+        parts = []
+        for hit in re.finditer(r'\d+', data[y]):
+            if abs(hit.start()-x) <= 1 or abs(hit.end()-x-1) <= 1:
+                parts.append(int(hit.group()))
+        return parts
     
+    possible_ys = [-1, 0, 1]
+    parts = []
+    for possible_y in possible_ys:
+        for part in check(y+possible_y,x):
+            if part != None:
+             parts.append(part)
+    assert len(parts) <= 2, f" Lenght is {len(parts)}"
+    if len(parts) == 2:
+            return parts[0]*parts[1]
+    else: 
+        return 0
 
+
+
+           
+parts2 = []
+
+for y, line in enumerate(data):
+    
+    for hit in re.finditer(r'\*',line):
+        x = hit.start()
+        parts2.append(find_parts(y,x))
+
+print(sum(parts2))
+
+
+ 
---
+++ 2023/day 4.py
@@ -0,0 +1,25 @@
+with open("day 4 input.txt") as my_file:
+    data = my_file.read().splitlines()
+
+def score(nbr):
+    if nbr > 0:
+        return pow(2,nbr-1)
+    else:
+        return 0
+
+def parse(line):
+    line = line.split(":")[1]
+    winners, numbers = line.split(" | ")
+    winners = set(winners.split())
+    numbers = set(numbers.split())
+    res = len(winners) + len(numbers) - len(winners | numbers)
+    if res != None:
+        return res
+    else:
+        return 0
+    
+sump1 = []
+for line in data:
+    sump1.append(score(parse(line)))
+
+print(sum(sump1))
