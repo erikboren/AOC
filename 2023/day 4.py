e34ede96325852eefd629a391aa1c1675b376b65
erikboren <erik.boren@gmail.com>
2023-12-14 22:11:43 +0100

    day 4 p2

--- 2023/day 4.py
+++ 2023/day 4.py
@@ -12,14 +12,23 @@ def parse(line):
     winners, numbers = line.split(" | ")
     winners = set(winners.split())
     numbers = set(numbers.split())
-    res = len(winners) + len(numbers) - len(winners | numbers)
+    res = len(winners) + len(numbers) -  len(winners | numbers)
     if res != None:
         return res
     else:
         return 0
     
 sump1 = []
+matches = []
 for line in data:
     sump1.append(score(parse(line)))
-
+    matches.append(parse(line))
 print(sum(sump1))
+
+# part 2
+cards = [1] * len(data)
+for i, score in enumerate(matches):
+    for k in range(1,score+1):
+        cards[i+k] = cards[i+k] + cards[i]
+
+print(sum(cards))
